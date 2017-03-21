51f173224d7d13e156807aca35d705f093a1668d
Snorg <shalmon.niv@gmail.com>
2017-03-21 14:25:56 +0200

    duplicate FRUC

---
+++ duplicateFRUC.m
@@ -0,0 +1,24 @@
+function [ new_video ] = duplicateFRUC( video, ratio )
+%solves the FRUC problem by duplicating frames
+
+    [height,width,original_frame_rate] = size(video);
+    new_frame_rate = original_frame_rate * ratio;
+    new_video = zeros(height,width,new_frame_rate);
+    counter = 1;
+    frame = 1;
+
+    disp(original_frame_rate)
+    
+    for i=1:1:new_frame_rate
+        new_video( :, :, i) = video( :, :, frame);
+        if counter == ratio
+            counter = 1;
+            frame = frame + 1;
+        else
+            counter = counter + 1;
+        end
+    end
+    
+    disp(frame)
+end
+
