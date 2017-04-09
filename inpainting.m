652f520d8210f1039c2a6645f0beb43d7e53881e
dormaayn <dor.d.ma@gmail.com>
2017-04-09 13:09:03 +0300

    First implementation of the inpainting algorithm

--- inpainting.m
+++ inpainting.m
@@ -1,10 +1,10 @@
 function [ restored ] = inpainting( corrupted, mask)
-%     prev = corrupted;
-%     for i=1:1:20,
-%         imwrite(prev,''InpaintingCycles/''
-%     end
-% 
-%     end
-     imwrite(corrupted,'InpaintingCycles/pic.jp2','jp2','CompressionRatio',300);
+    prev = corrupted;
+    for i=1:1:40,
+       imwrite(prev,strcat(strcat('InpaintingCycles/pic',int2str(i)),'.jp2'),'jp2','CompressionRatio',1000-15*i);
+       res = imread(strcat(strcat('InpaintingCycles/pic',int2str(i)),'.jp2'));
+       prev = (uint8(res) .* uint8(zeros(size(res))+mask)) + uint8(corrupted);
+    end
+    restored = prev;
 end
 
