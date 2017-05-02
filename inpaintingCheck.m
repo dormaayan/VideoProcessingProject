4fa96a1a569e94696ea2c1be8547fef049be02dd
dormaayn <dor.d.ma@gmail.com>
2017-05-02 11:03:57 +0300

    Added New Masks & Checks

--- inpaintingCheck.m
+++ inpaintingCheck.m
@@ -1,11 +1,34 @@
-pic = load('lena_1.mat');
-imshow(x,[0 255]);
+Horizontalpasses = zeros(512,512);
+for i=1:1:512
+    for j=1:1:512
+        if mod(i,20)<5
+            Horizontalpasses(i,j)=1;
+        end 
+    end
+end
 figure;
-imshow(mask,[0 1]);
- corrupted = x .* (ones(size(x))-mask);
+imshow(Horizontalpasses,[0 1]);
+
+Verticalpasses = zeros(512,512);
+for i=1:1:512
+    for j=1:1:512
+        if mod(j,20)<5
+            Verticalpasses(i,j)=1;
+        end 
+    end
+end
+figure;
+imshow(Verticalpasses,[0 1]);
+
+ pic = load('lena_1.mat');
+ figure;
+ imshow(x,[0 255]);
+% figure;
+% imshow(mask,[0 1]);
+ corrupted = x .* (ones(size(x))-Verticalpasses);
  figure;
  imshow(corrupted,[0 255]);
-result = inpainting(uint8(corrupted),mask);
+result = inpainting(uint8(corrupted),Verticalpasses);
  figure;
  imshow(result,[0 255]);
 
