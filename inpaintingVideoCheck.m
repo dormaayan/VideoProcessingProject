f58b28eb3de9a383e41743ee9075504e2ef44ff9
dormaayn <dor.d.ma@gmail.com>
2017-05-18 09:05:41 +0300

    last version

--- inpaintingVideoCheck.m
+++ inpaintingVideoCheck.m
@@ -1,24 +1,27 @@
-% fprintf('\n ~~~~~~~Analyzing Tv Video~~~~~~~ \n');
-% [original,imp] = loadVideo('Videos/tv.yuv',176,144,300);
-% mask = zeros([1,300]);
-% for i=1:2:300
-%     mask(i)=1;
-% end
-% filtered = original(:,:,2:2:300);
-% % new_vid = averageFRUC(filtered);
-% new_inpainting_vid = inpaintingFRUCHorizontal(filtered);
-% [mse,psnr]=errorsVideos(original,new_inpainting_vid,mask);
-% fprintf('\n The Mse is: %d \n',mse);
-% fprintf('\n The PSNR is: %d \n',psnr);
-
-fprintf('\n ~~~~~~~Analyzing Ducks Take Off Video~~~~~~~ \n');
-[original,imp] = loadVideo('Videos/ducks.yuv',1280,720,500);
-mask = zeros([1,500]);
-for i=1:2:500
+fprintf('\n ~~~~~~~Analyzing Tv Video~~~~~~~ \n');
+[original,imp] = loadVideo('Videos/tv.yuv',176,144,300);
+mask = zeros([1,300]);
+for i=1:2:300
     mask(i)=1;
 end
- filtered = original(:,:,2:2:500);
-new_inpainting_vid = inpaintingFRUCHorizontal(filtered);
- [mse,psnr]=errorsVideos(original,new_inpainting_vid,mask);
+filtered = original(:,:,2:2:300);
+% new_vid = averageFRUC(filtered);
+% new_inpainting_vid = inpaintingFRUCHorizontal(filtered);
+new_inpainting_vid = inpaintingFRUCVertical(filtered);
+figure;
+imshow(new_inpainting_vid(:,:,164),[0 254]);
+[mse,psnr]=errorsVideos(original,new_inpainting_vid,mask);
 fprintf('\n The Mse is: %d \n',mse);
 fprintf('\n The PSNR is: %d \n',psnr);
+
+% fprintf('\n ~~~~~~~Analyzing Ducks Take Off Video~~~~~~~ \n');
+% [original,imp] = loadVideo('Videos/ducks.yuv',1280,720,500);
+% mask = zeros([1,500]);
+% for i=1:2:500
+%     mask(i)=1;
+% end
+%  filtered = original(:,:,2:2:500);
+% new_inpainting_vid = inpaintingFRUCHorizontal(filtered);
+%  [mse,psnr]=errorsVideos(original,new_inpainting_vid,mask);
+% fprintf('\n The Mse is: %d \n',mse);
+% fprintf('\n The PSNR is: %d \n',psnr);
