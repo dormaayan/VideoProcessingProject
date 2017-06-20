16f90f8686a151093820107a4a5fe9cc1ca5f671
dormaayn <dor.d.ma@gmail.com>
2017-06-20 17:17:55 +0300

    sync

--- inpaintingFRUCAlternating.m
+++ inpaintingFRUCAlternating.m
@@ -29,28 +29,16 @@ function [ new ] = inpaintingFRUCAlternating( original )
     beta = 0.001;
     new = corrupted;
     for i=1:1:50,
-%         for j=1:1:height,
-%             img = permute(new(j,:,:),[2 3 1]);
-%             res = compressDecompress(img,i,max((2*myu)/beta,1));
-%             new(j,:,:) = (uint8(res) .* uint8(maskW)) + permute(uint8(corrupted(j,:,:)),[2 3 1]);
-% %              if i==2 && j==100,
-% %                 figure;
-% %                 imshow(img,[0 255]);
-% %                  figure;
-% %                 imshow(permute(new(j,:,:),[2 3 1]),[0 255]);
-% %             end
-%         end
+        for j=1:1:height,
+            img = permute(new(j,:,:),[2 3 1]);
+            res = compressDecompress(uint8(img),i,max((2*myu)/beta,1));
+            new(j,:,:) = (uint8(res) .* uint8(maskW)) + permute(uint8(corrupted(j,:,:)),[2 3 1]);
+        end
         
        for j=1:1:width,
             img = permute(new(:,j,:),[1 3 2]);
-            res = compressDecompress(img,i,max((2*myu)/beta,1));
+            res = compressDecompress(uint8(img),i,max((2*myu)/beta,1));
             new(:,j,:) = (uint8(res) .* uint8(maskH)) + permute(uint8(corrupted(:,j,:)),[1 3 2]);
-            if i==40 && j==100,
-                figure;
-                imshow(img,[0 255]);
-                 figure;
-                imshow(permute(new(:,j,:),[1 3 2]),[0 255]);
-            end
        end
        
        beta = 1.1*beta;
