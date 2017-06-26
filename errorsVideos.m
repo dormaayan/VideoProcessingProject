7ca53a53c0eba05d7e186c711e2192f12b2e65ec
Niv Shalmon <shalmon.niv@gmail.com>
2017-06-26 15:10:45 +0300

    fix psnr problem and average/duplicate fruc

--- errorsVideos.m
+++ errorsVideos.m
@@ -1,11 +1,14 @@
 
 function [ mse,psnrErr ] = errorsVideos( origin, restored, mask)
-totalMse=0;
-totalPsnr = 0;
+totalMse = 0.0;
+totalPsnr = 0.0;
 for i=1:1:max(size(mask))
     if mask(i)==1
-    totalMse = totalMse + mask(i) * immse(uint8(origin(:,:,i)),uint8(restored(:,:,i)));
-    totalPsnr = totalPsnr + mask(i) * psnr(uint8(origin(:,:,i)),uint8(restored(:,:,i)),255);
+        totalMse = totalMse + immse(uint8(origin(:,:,i)),uint8(restored(:,:,i)));
+        temp = psnr(uint8(origin(:,:,i)),uint8(restored(:,:,i)),255);
+        if temp ~= Inf
+            totalPsnr = totalPsnr + temp;
+        end
     end
 end
 mse = double(totalMse)/double(sum(mask));
