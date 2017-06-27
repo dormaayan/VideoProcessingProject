64c6c546b687337864930a356fb0075ca6329b7e
Niv Shalmon <shalmon.niv@gmail.com>
2017-06-27 10:40:41 +0300

    horizontal mses graph

--- inpaintingFRUCHorizontal.m
+++ inpaintingFRUCHorizontal.m
@@ -1,4 +1,4 @@
-function [ new ] = inpaintingFRUCHorizontal( original )
+function [ new ] = inpaintingFRUCHorizontal( original, comparison, mask )
     [height,width,original_frame_rate] = size(original);
     new = averageFRUC(original);
     
@@ -10,13 +10,20 @@ function [ new ] = inpaintingFRUCHorizontal( original )
         end 
     end
     end
-    for i=1:1:width,
-        disp(i);
-        img = permute(new(:,i,:),[1 3 2]);
-        inpainted = inpainting(img,mask,0.3);
-        new(:,i,:) = inpainted;
-    end
-
-
+    beta = 0.3;
+    myu = 3;
+    itr = 100;
+    mses = zeroes(itr);
+    for i=1:1:itr,
+       disp(i);
+       for j=1:1:width,
+        img = permute(new(:,j,:),[1 3 2]);
+        res = compressDecompress(uint8(img),i,max((2*myu)/beta,1));
+        new(:,j,:) = (uint8(res) .* uint8(mask)) + (uint8(img) .* uint8(1-mask));
+       end
+       mses(i) = errorVideos(comparison, mses, mask);
+       beta = 1.1*beta;
+    end 
+    plot(mses);
 end
 
