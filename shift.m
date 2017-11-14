6a22703f272adf7cb889e2a4a84c3dc0b48db44f
Niv Shalmon <shalmon.niv@gmail.com>
2017-11-14 19:38:45 +0200

    added shift methods back

---
+++ shift.m
@@ -0,0 +1,5 @@
+function [ shifted ] = shift(img,shifts,j)
+row = floor(j/shifts);
+column = mod(j,shifts);
+shifted = img((column+1):end,(row+1):end);
+end
---
+++ shiftBack.m
@@ -0,0 +1,8 @@
+function [ reShifted ] = shiftBack(shifted,shifts,j,h,w)
+
+reShifted = zeros(h,w);
+row = floor(j/shifts);
+column = mod(j,shifts);
+reShifted((column+1):end,(row+1):end) = shifted;
+
+end
